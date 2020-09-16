a34243a1853bb6ecd40011fd5fdefefce1c59e88
ynotveera <54206653+ynotveera@users.noreply.github.com>
2020-09-16 21:06:02 +0530

    Update FromInternet.java

--- googleSheetAPI/src/main/java/com/googlesheets/googleSheetAPI/FromInternet.java
+++ googleSheetAPI/src/main/java/com/googlesheets/googleSheetAPI/FromInternet.java
@@ -1,15 +1,24 @@
 package com.googlesheets.googleSheetAPI;
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.AddSheetRequest;
 import com.google.api.services.sheets.v4.model.BatchUpdateSpreadsheetRequest;
+import com.google.api.services.sheets.v4.model.CellData;
+import com.google.api.services.sheets.v4.model.ExtendedValue;
+import com.google.api.services.sheets.v4.model.GridCoordinate;
 import com.google.api.services.sheets.v4.model.Request;
+import com.google.api.services.sheets.v4.model.RowData;
 import com.google.api.services.sheets.v4.model.SheetProperties;
 import com.google.api.services.sheets.v4.model.Spreadsheet;
 import com.google.api.services.sheets.v4.model.SpreadsheetProperties;
+import com.google.api.services.sheets.v4.model.UpdateCellsRequest;
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Data;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.Sheets.Spreadsheets;
 import com.google.api.services.sheets.v4.SheetsRequestInitializer;
@@ -23,102 +32,131 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FromInternet {
-	
-	public static void main(String[] args) throws IOException, GeneralSecurityException
-	{
-		GoogleCredential credential = GoogleCredential.fromStream(Files.newInputStream(Paths.get("C:\\Users\\91720\\Downloads\\secret.json")))
-				.createScoped(Arrays.asList(SheetsScopes.DRIVE));
-		
-		Sheets.Spreadsheets spreadsheets = new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(),
-                credential)
-                .setApplicationName("A")
-                .build()
-                .spreadsheets();
-		
-		List<List<Object>> sheet = spreadsheets.values().get("1-i_zvIBXbDCbqElhh-o-GcmsXTHByyidzQ_cX6rOOiY", "Sheet1!a1:a2")
-                .setMajorDimension("ROWS")
-                .setValueRenderOption("UNFORMATTED_VALUE")
-                .setDateTimeRenderOption("FORMATTED_STRING")
-                .execute()
-                .getValues();  
 
-		for (List row : sheet) {
+	static Sheets.Spreadsheets spreadsheets;
+	static GoogleCredential credential;
+	static final String existingSpreadSheetID = "1vIKnhokR94w6cur7PTf4I_y45xXwTDesuGHJixHk_M0";
 
-            // Print columns A and E, which correspond to indices 0 and 4.
+	public static void main(String[] args) throws IOException, GeneralSecurityException {
 
-           // System.out.printf("%s, %s\n", row.get(0));
-			System.out.println("AA");
-
-          }
-		
-		 //Set sheet name
-        //Can be any string, I chose to set it to the account name
-        //String sheetName = mCredential.getSelectedAccountName();
-
-        //Create a new AddSheetRequest
-        AddSheetRequest addSheetRequest = new AddSheetRequest();
-        SheetProperties sheetProperties = new SheetProperties();
-
-        //Add the sheetName to the sheetProperties
-        addSheetRequest.setProperties(sheetProperties);
-        addSheetRequest.setProperties(sheetProperties.setTitle("Hi"));
-
-        //Create batchUpdateSpreadsheetRequest
-        BatchUpdateSpreadsheetRequest batchUpdateSpreadsheetRequest = new BatchUpdateSpreadsheetRequest();
-
-        //Create requestList and set it on the batchUpdateSpreadsheetRequest
-        List<Request> requestsList = new ArrayList<Request>();
-        batchUpdateSpreadsheetRequest.setRequests(requestsList);
-
-        //Create a new request with containing the addSheetRequest and add it to the requestList
-        Request request = new Request();
-        request.setAddSheet(addSheetRequest);
-        requestsList.add(request);
-
-        //Add the requestList to the batchUpdateSpreadsheetRequest
-        batchUpdateSpreadsheetRequest.setRequests(requestsList);
-
-        //Call the sheets API to execute the batchUpdate
-        spreadsheets.batchUpdate("1-i_zvIBXbDCbqElhh-o-GcmsXTHByyidzQ_cX6rOOiY",batchUpdateSpreadsheetRequest).execute();
-		
-		/*// The spreadsheet to apply the updates to.
-	    String spreadsheetId = "1-i_zvIBXbDCbqElhh-o-GcmsXTHByyidzQ_cX6rOOiY"; // TODO: Update placeholder value.
-
-	    // A list of updates to apply to the spreadsheet.
-	    // Requests will be applied in the order they are specified.
-	    // If any request is not valid, no requests will be applied.
-	    List<Request> requests = new ArrayList<Request>(); // TODO: Update placeholder value.
-	    
-	    Request req = new Request();
-	    req.getAddSheet();
-	    req.setAddSheet(addSheet)
-
-	    requests.add(req);
-	    //requests.add("")
-	    // TODO: Assign values to desired fields of `requestBody`:
-	    BatchUpdateSpreadsheetRequest requestBody = new BatchUpdateSpreadsheetRequest();
-	    requestBody.setRequests(requests);
-
-	    Sheets.Spreadsheets.BatchUpdate request =
-	    		spreadsheets.batchUpdate(spreadsheetId, requestBody);*/
-		
-		/*Spreadsheet requestBody = new Spreadsheet();
-	    Sheets.Spreadsheets.Create request = spreadsheets.create(requestBody);
-
-	    Spreadsheet response = request.execute();*/
-		
-		/*Spreadsheet spreadSheet = new Spreadsheet().setProperties(
-			      new SpreadsheetProperties().setTitle("My Spreadsheet"));
-			    //Spreadsheet result = sheetsService.spreadsheets().create(spreadSheet).execute();
-		spreadsheets.create(spreadSheet).execute();*/
-		
-		//System.out.println(spreadsheets.get);
-			        
-			    //assertThat(spreadsheets1.getSpreadsheetId()).isNotNull();  
-            // Print columns A and E, which correspond to indices 0 and 4.
+		authorize("C:\\Users\\vigneshram_s\\Downloads\\selenium-project-289514-438b4a0157eb.json");
+		getSpreadsheetInstance();
+		//createNewSheet(existingSpreadSheetID, "Hi12");
+		//writevaluesinSheetType1(existingSpreadSheetID);		
+		//writeSomething();
+		//getRows();
 		System.out.println("AA");
-		
+
 	}
 
+	public static void authorize(String credentialPath) throws IOException {
+		credential = GoogleCredential.fromStream(Files.newInputStream(Paths.get(credentialPath)))
+				.createScoped(Arrays.asList(SheetsScopes.DRIVE));
+	}
+
+	public static void getSpreadsheetInstance() throws GeneralSecurityException, IOException {
+		spreadsheets = new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+				JacksonFactory.getDefaultInstance(), credential).setApplicationName("A").build().spreadsheets();
+	}
+
+	public static List<List<Object>> getSpreadsheetValues(String sheetID, String range) throws IOException {
+		return spreadsheets.values().get(sheetID, range).setMajorDimension("ROWS")
+				.setValueRenderOption("UNFORMATTED_VALUE").setDateTimeRenderOption("FORMATTED_STRING").execute()
+				.getValues();
+	}
+
+	public static void createNewSheet(String existingSpreadSheetID, String newsheetTitle) throws IOException {
+		// Create a new AddSheetRequest
+		AddSheetRequest addSheetRequest = new AddSheetRequest();
+		SheetProperties sheetProperties = new SheetProperties();
+		sheetProperties.setIndex(0);
+
+		// Add the sheetName to the sheetProperties
+		addSheetRequest.setProperties(sheetProperties);
+		addSheetRequest.setProperties(sheetProperties.setTitle(newsheetTitle));
+
+		// Create batchUpdateSpreadsheetRequest
+		BatchUpdateSpreadsheetRequest batchUpdateSpreadsheetRequest = new BatchUpdateSpreadsheetRequest();
+
+		// Create requestList and set it on the batchUpdateSpreadsheetRequest
+		List<Request> requestsList = new ArrayList<Request>();
+		batchUpdateSpreadsheetRequest.setRequests(requestsList);
+
+		// Create a new request with containing the addSheetRequest and add it to the
+		// requestList
+		Request request = new Request();
+		request.setAddSheet(addSheetRequest);
+		requestsList.add(request);
+
+		// Add the requestList to the batchUpdateSpreadsheetRequest
+		batchUpdateSpreadsheetRequest.setRequests(requestsList);
+
+		// Call the sheets API to execute the batchUpdate
+		spreadsheets.batchUpdate(existingSpreadSheetID, batchUpdateSpreadsheetRequest).execute();
+	}
+
+	public static void writevaluesinSheetType1(String existingSpreadSheetID) throws IOException {
+
+		Request req = new Request();
+		List<Request> requests = new ArrayList<Request>();
+		requests.add(req);
+		
+		ExtendedValue extendedValue = new ExtendedValue();
+		extendedValue.setStringValue("Hello World");
+		
+		CellData cellData = new CellData();
+		cellData.setUserEnteredValue(extendedValue);
+		
+		List<CellData> values = new ArrayList<CellData>();
+		values.add(cellData);
+		
+		RowData data = new RowData();
+		data.setValues(values);
+
+		GridCoordinate grids = new GridCoordinate();
+		grids.setSheetId(0);
+		grids.setRowIndex(1);
+		grids.setColumnIndex(1);
+		
+		UpdateCellsRequest updateCellReq = new UpdateCellsRequest();
+		updateCellReq.setStart(grids);
+		updateCellReq.setFields("userEnteredValue,userEnteredFormat.backgroundColor");
+		req.setUpdateCells(updateCellReq);
+		
+
+		List<RowData> rowData = new ArrayList<RowData>();
+		rowData.add(data);
+		updateCellReq.setRows(rowData);
+		
+		requests.add(req);
+
+		BatchUpdateSpreadsheetRequest batchUpdateRequest = new BatchUpdateSpreadsheetRequest().
+				setRequests(requests);
+		spreadsheets.batchUpdate(existingSpreadSheetID, batchUpdateRequest).execute();
+
+	}
+	
+	public static void writeSomething(List<Object> inputData) throws IOException {
+
+		/*List<Object> innerList = new ArrayList<Object>();
+		innerList.add("aaaaaaaaa");
+	*/
+		List<List<Object>> values = Arrays.asList(inputData);
+		ValueRange body = new ValueRange()
+		        .setValues(values);
+		UpdateValuesResponse result =
+		        spreadsheets.values().update(existingSpreadSheetID, "Hi1!A1", body)
+		                .setValueInputOption("RAW")
+		                .execute();
+		System.out.printf("%d cells updated.", result.getUpdatedCells());
+
+	   
+	}
+	
+	public static void getRows() throws IOException
+	{
+		ValueRange result = spreadsheets.values().get(existingSpreadSheetID, "Sheet1").execute();
+		int numRows = result.getValues() != null ? result.getValues().size() : 0;
+		System.out.printf("%d rows retrieved.", numRows);
+	}
 }
